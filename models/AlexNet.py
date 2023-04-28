4ae93824ff6232d96185292b35d3f7b987baaf49
PL97 <pengle@126.com>
2023-04-28 11:34:35 -0500

    add localresponsenorm layer to alexnet

--- models/AlexNet.py
+++ models/AlexNet.py
@@ -27,9 +27,11 @@ class AlexNet(shared):
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=2),
             nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
             nn.MaxPool2d(kernel_size=2),
             nn.Conv2d(64, 192,kernel_size=3, padding=2),
             nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
             nn.MaxPool2d(kernel_size=2),
             nn.Conv2d(192, 384, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
