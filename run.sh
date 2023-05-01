59eabe9e34c5f6ebfc0a6a1af3ff2ec80171850b
PL97 <pengle@126.com>
2023-04-30 23:12:10 -0500

    move fig to figs/

--- run.sh
+++ run.sh
@@ -16,25 +16,24 @@
 #                 --sto 0
 
 
-python main.py --run_name constrained_IL \
+python main.py --run_name constrained_IL_NIPS \
                 --dataset cifar100 \
                 --model MLP \
-                --batch_size 8000 \
+                --batch_size 4000 \
                 --learning_rate 1e-4 \
-                --learning_rate_s 1e-2 \
+                --learning_rate_s 5e-2 \
                 --method AL_FPOR \
                 --subprob_max_epoch 100 \
                 --rounds 50 \
                 --alpha 0.99 \
                 --t 0.5 \
                 --solver AdamW \
-                --warm_start 30 \
+                --warm_start 0 \
                 --rho 1 \
                 --delta 1 \
                 --sto 1 \
                 --reg 0.1 \
                 --saved_fig_name 01_ws
-
 
 
 # CUDA_VISIBLE_DEVICES=0 python main.py --run_name WCE_trail1_s \
